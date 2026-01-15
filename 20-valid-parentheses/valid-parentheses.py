8a03f30cda7d870d02c55e2a452c28cd80e394df
mmann2511 <michaelgmann25@gmail.com>
2026-01-15 08:29:24 -0600

    Time: 3 ms (37.00%) | Memory: 19.4 MB (10.25%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,17 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # Return true or false
+        # Valid if open brackets must be closed by same type of brackets
+        # Open brackets must be closed in the correct order
+        # Every close bracket has a corresponding open brack of the same type
+        # Dictionary and LIFO Stack
+        pairs = {')': '(', '}': '{', ']': '['}
         stack = []
-        opening = {'(', '{', '['}
-        mapping = {')': '(', '}': '{', ']': '['}
-    
+
         for char in s:
-            if char in opening:
+            # if character is an opening value
+            if char in pairs.values():
                 stack.append(char)
-            else:  # closing bracket
-                if not stack or stack.pop() != mapping[char]:
+            # if character is a closing value
+            else:
+                if not stack:
                     return False
-    
-        return not stack               
+                if stack[-1] != pairs[char]:
+                    return False
+                stack.pop()
 
+        return len(stack) == 0                   
 
-        
