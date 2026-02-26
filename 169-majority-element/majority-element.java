ccb48b75183e27a51ad9ff2739dc31d44f0496d6
mmann2511 <michaelgmann25@gmail.com>
2026-02-26 10:08:51 -0600

    Time: 2 ms (70.69%) | Memory: 55.1 MB (73.43%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-
-        return nums[nums.length/2];
+       int candidate = 0, count = 0;
+       for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+       }
+       return candidate;
     }
 }
