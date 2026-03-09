d7543e8dc0fdebcb65b77a4e35b272e8cf3acf0e
mmann2511 <michaelgmann25@gmail.com>
2026-03-09 14:47:53 -0600

    Time: 3 ms (79.20%) | Memory: 43.2 MB (52.31%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -9,10 +9,10 @@ class Solution {
         
         int start = 0;
 
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i <= nums.length; i++) {
             int rear = i - 1;
 
-            if (nums[i] != nums[rear] + 1) {
+            if (i == nums.length || nums[i] != nums[rear] + 1) {
                 if (nums[start] == nums[rear]) {
                     result.add("" + nums[start]);
                     start = i;
@@ -23,17 +23,7 @@ class Solution {
                     start = i;
                 }
             } 
-        }    
-
-        int rear = nums.length - 1;
-        if (nums[start] == nums[rear]) {
-            result.add("" + nums[start]);
-        }
-        else if (nums[start] != nums[rear]) {
-            result.add(nums[start] + "->" + nums[rear]);
-        }    
-
-        
+        }      
         return result;
     }
 }
