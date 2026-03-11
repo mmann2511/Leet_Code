7fbaaa54c5e73dbac097437d66e352bd6b5c057c
mmann2511 <michaelgmann25@gmail.com>
2026-03-11 09:24:09 -0600

    Time: 1 ms (97.90%) | Memory: 42.5 MB (79.19%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,5 +1,13 @@
 class Solution {
     public int addDigits(int num) {
-        return num == 0 ? 0 : (num % 9 == 0 ? 9 : num % 9);
+        if (num == 0) {
+            return 0;
+        }
+        else if (num % 9 == 0) {
+            return 9;
+        }
+        else {
+            return num % 9;
+        }
     }
 }
