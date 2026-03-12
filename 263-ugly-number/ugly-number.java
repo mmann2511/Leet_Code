a7cfb0f832a2ba789404e94d708a0fcb61ec58c4
mmann2511 <michaelgmann25@gmail.com>
2026-03-12 08:47:06 -0600

    Time: 1 ms (99.25%) | Memory: 42.7 MB (49.60%) - LeetSync

---
+++ 263-ugly-number/ugly-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isUgly(int n) {
+        // positive integer which does NOT have a prime factor
+        // other than 2, 3, 5
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+
+        
+
+        return true ? n == 1 : false;
+    }
+}
