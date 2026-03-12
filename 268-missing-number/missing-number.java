be4530d6351f27e698b3f8adf1db3c5e68ca2e9b
mmann2511 <michaelgmann25@gmail.com>
2026-03-12 09:14:55 -0600

    Time: 0 ms (100.00%) | Memory: 47.3 MB (63.90%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        int max = (nums.length * (nums.length + 1)) / 2;
+
+        return max - sum;
+
+        
+    }
+}
