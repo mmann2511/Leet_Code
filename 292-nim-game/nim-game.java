23081b344ecd91956066c86e263abfe24485a64d
mmann2511 <michaelgmann25@gmail.com>
2026-03-13 10:01:08 -0600

    Time: 0 ms (100.00%) | Memory: 42.2 MB (50.98%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if (n == 0) {
-            return false;
-        }
-        if (n == 1 || n == 2 || n == 3) {
-            return true;
-        }
-
-        if (n % 4 == 0) {
-            return false;
-        }
-        return true;
-    }
+        return n % 4 != 0;
+    }    
 }
