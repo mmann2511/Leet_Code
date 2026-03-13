c10752ba81f82e045a0f102f26cd10002841cf59
mmann2511 <michaelgmann25@gmail.com>
2026-03-13 08:36:16 -0600

    Time: 2 ms (90.43%) | Memory: 47.9 MB (34.04%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int insertPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                continue;
+
+            }
+            else {
+                nums[insertPos++] = nums[i];
+            }
+        }
+
+        for (int i = insertPos; i < nums.length; i++) {
+            nums[i] = 0;
+            insertPos--;
+        }
+    }
+}
